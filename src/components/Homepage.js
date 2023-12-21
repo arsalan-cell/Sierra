fd32be907eb92b46667895ad722f71e91eac9fcd
arsalan <arsalancity61@gmail.com>
2023-12-21 19:21:02 +0500

    sections

--- src/components/Homepage.js
+++ src/components/Homepage.js
@@ -5,10 +5,11 @@ const Homepage = () => {
 
     
   return (
+   
    <section>
     <div className='min-h-[100vh] border-2 hero_bg'>
-    <div className='absolute text-white text-center mt-40 ml-80 '>
-        <h2 className='text-7xl'><span class="text-green-400">Streamline</span> your projects,</h2>
+    <div className='absolute text-white text-center mt-40 ml-96 '>
+        <h2 className='text-7xl'><span className="text-green-400">Streamline</span> your projects,</h2>
         <h2 className='text-7xl'>empower your team</h2>
         <h2 className='text-2xl mt-14'>Efficient planning,seamless collaboration, and top-notch</h2>
         <h2 className='text-2xl'>data protection-all in one place</h2>
@@ -24,55 +25,55 @@ const Homepage = () => {
    
    
    
-   
+    
     <div className='h-screen w-screen bg-slate-100'>
-    <section class="text-gray-600 body-font">
-  <div class="container px-5 py-24 mx-auto">
-    <div class="flex flex-col text-center w-full mb-20">
-      <h5 class="sm:text-1xl mb-4 text-gray-900">Project management essentials</h5>
-      <h1 class=" sm:text-5xl mb-4 text-gray-900">Plan,execute,and track</h1>
-      <h5 class="sm:text-5xl mb-4 text-gray-900">projects of <span className='text-green-400'>any size</span></h5>
-      <h5 class="lg:w-2/3 mx-auto leading-relaxed text-base">Our app offers solid planning tools,seamless collaboration features,robust data protection,and unparalleled software support</h5>
+    <section className="text-gray-600 body-font">
+  <div className="container px-5 py-24 mx-auto">
+    <div className="flex flex-col text-center w-full mb-20">
+      <h5 className="sm:text-1xl mb-4 text-gray-900">Project management essentials</h5>
+      <h1 className=" sm:text-5xl mb-4 text-gray-900">Plan,execute,and track</h1>
+      <h5 className="sm:text-5xl mb-4 text-gray-900">projects of <span className='text-green-400'>any size</span></h5>
+      <h5 className="lg:w-2/3 mx-auto leading-relaxed text-base">Our app offers solid planning tools,seamless collaboration features,robust data protection,and unparalleled software support</h5>
     </div>
-    <div class="flex flex-wrap -m-4 text-center">
-      <div class="p-4 md:w-1/4 sm:w-1/2 w-full">
-        <div class="border-0 border-gray-200 px-4 py-6 rounded-lg">
-          <svg fill="none" stroke="currentColor" stroke-linecap="round" stroke-linejoin="round" stroke-width="2" class="text-indigo-500 hover:text-green-400 w-12 h-12 mb-3 inline-block" viewBox="0 0 24 24">
+    <div className="flex flex-wrap -m-4 text-center">
+      <div className="p-4 md:w-1/4 sm:w-1/2 w-full">
+        <div className="border-0 border-gray-200 px-4 py-6 rounded-lg">
+          <svg fill="none" stroke="currentColor" stroke-linecap="round" stroke-linejoin="round" stroke-width="2" className="text-indigo-500 hover:text-green-400 w-12 h-12 mb-3 inline-block" viewBox="0 0 24 24">
             <path d="M8 17l4 4 4-4m-4-5v9"></path>
             <path d="M20.88 18.09A5 5 0 0018 9h-1.26A8 8 0 103 16.29"></path>
           </svg>
-          <h2 class="text-left title-font font-bold text-1xl  text-gray-900">Solid planning</h2>
-          <p class=" text-left leading-relaxed">From the small stuff to the big picture ,we organize work</p>
+          <h2 className="text-left title-font font-bold text-1xl  text-gray-900">Solid planning</h2>
+          <p className=" text-left leading-relaxed">From the small stuff to the big picture ,we organize work</p>
         </div>
       </div>
-      <div class="p-4 md:w-1/4 sm:w-1/2 w-full">
-        <div class="border-0 border-gray-200 px-4 py-6 rounded-lg">
-          <svg fill="none" stroke="currentColor" stroke-linecap="round" stroke-linejoin="round" stroke-width="2" class="text-indigo-500 hover:text-green-400 w-12 h-12 mb-3 inline-block" viewBox="0 0 24 24">
+      <div className="p-4 md:w-1/4 sm:w-1/2 w-full">
+        <div className="border-0 border-gray-200 px-4 py-6 rounded-lg">
+          <svg fill="none" stroke="currentColor" stroke-linecap="round" stroke-linejoin="round" stroke-width="2" className="text-indigo-500 hover:text-green-400 w-12 h-12 mb-3 inline-block" viewBox="0 0 24 24">
             <path d="M17 21v-2a4 4 0 00-4-4H5a4 4 0 00-4 4v2"></path>
             <circle cx="9" cy="7" r="4"></circle>
             <path d="M23 21v-2a4 4 0 00-3-3.87m-4-12a4 4 0 010 7.75"></path>
           </svg>
-          <h2 class=" text-left title-font font-bold text-1xl text-gray-900">Collaboration tools</h2>
-          <p class="text-left leading-relaxed">From the small stuff to the big picture ,we organize work</p>
+          <h2 className=" text-left title-font font-bold text-1xl text-gray-900">Collaboration tools</h2>
+          <p className="text-left leading-relaxed">From the small stuff to the big picture ,we organize work</p>
         </div>
       </div>
-      <div class="p-4 md:w-1/4 sm:w-1/2 w-full">
-        <div class="border-0 border-gray-200 px-4 py-6 rounded-lg">
-          <svg fill="none" stroke="currentColor" stroke-linecap="round" stroke-linejoin="round" stroke-width="2" class=" text-indigo-500 hover:text-green-400 w-12 h-12 mb-3 inline-block" viewBox="0 0 24 24">
+      <div className="p-4 md:w-1/4 sm:w-1/2 w-full">
+        <div className="border-0 border-gray-200 px-4 py-6 rounded-lg">
+          <svg fill="none" stroke="currentColor" stroke-linecap="round" stroke-linejoin="round" stroke-width="2" className=" text-indigo-500 hover:text-green-400 w-12 h-12 mb-3 inline-block" viewBox="0 0 24 24">
             <path d="M3 18v-6a9 9 0 0118 0v6"></path>
             <path d="M21 19a2 2 0 01-2 2h-1a2 2 0 01-2-2v-3a2 2 0 012-2h3zM3 19a2 2 0 002 2h1a2 2 0 002-2v-3a2 2 0 00-2-2H3z"></path>
           </svg>
-          <h2 class="text-left title-font font-bold text-1xl text-gray-900">Data protection</h2>
-          <p class="text-left leading-relaxed">From the small stuff to the big picture ,we organize work</p>
+          <h2 className="text-left title-font font-bold text-1xl text-gray-900">Data protection</h2>
+          <p className="text-left leading-relaxed">From the small stuff to the big picture ,we organize work</p>
         </div>
       </div>
-      <div class="p-4 md:w-1/4 sm:w-1/2 w-full">
-        <div class="border-0 border-gray-200 px-4 py-6 rounded-lg">
-          <svg fill="none" stroke="currentColor" stroke-linecap="round" stroke-linejoin="round" stroke-width="2" class="text-indigo-500 hover:text-green-400 w-12 h-12 mb-3 inline-block" viewBox="0 0 24 24">
+      <div className="p-4 md:w-1/4 sm:w-1/2 w-full">
+        <div className="border-0 border-gray-200 px-4 py-6 rounded-lg">
+          <svg fill="none" stroke="currentColor" stroke-linecap="round" stroke-linejoin="round" stroke-width="2" className="text-indigo-500 hover:text-green-400 w-12 h-12 mb-3 inline-block" viewBox="0 0 24 24">
             <path d="M12 22s8-4 8-10V5l-8-3-8 3v7c0 6 8 10 8 10z"></path>
           </svg>
-          <h2 class="text-left title-font font-bold text-1xl text-gray-900">Software support</h2>
-          <p class="text-left leading-relaxed">From the small stuff to the big picture ,we organize work</p>
+          <h2 className="text-left title-font font-bold text-1xl text-gray-900">Software support</h2>
+          <p className="text-left leading-relaxed">From the small stuff to the big picture ,we organize work</p>
         </div>
       </div>
     </div>
@@ -81,31 +82,120 @@ const Homepage = () => {
     </div>
 
     <div>
-    <section class="text-gray-600 body-font">
-  <div class="container mx-auto flex px-5 py-24 md:flex-row flex-col items-center">
-    <div class="lg:flex-grow md:w-1/2 lg:pr-24 md:pr-16 flex flex-col md:items-start md:text-left mb-16 md:mb-0 items-center text-center">
+    <section className="text-gray-600 body-font">
+  <div className="container mx-auto flex px-5 py-24 md:flex-row flex-col items-center">
+    <div className="lg:flex-grow md:w-1/2 lg:pr-24 md:pr-16 flex flex-col md:items-start md:text-left mb-16 md:mb-0 items-center text-center">
       <h1 className='font-bold text-black'>Real-time analytics</h1>
-      <h1 class="  sm:text-4xl text-3xl mb-4 font-large text-gray-900">Check project progress
-        <br class="hidden lg:inline-block"/>and <span className='text-green-400'>performance</span> 
+      <h1 className="  sm:text-4xl text-3xl mb-4 font-large text-gray-900">Check project progress
+        <br className="hidden lg:inline-block"/>and <span className='text-green-400'>performance</span> 
       </h1>
-      <p class="mb-8 leading-relaxed">Boost productivity,optimize workflows,and make  <br class="hidden lg:inline-block"/> data-driven decisions effortlessly</p>
+      <p className="mb-8 leading-relaxed">Boost productivity,optimize workflows,and make  <br className="hidden lg:inline-block"/> data-driven decisions effortlessly</p>
       <p className='font-bold text-black'>Performance tracking</p>
       <p className='font-bold text-black'>Resource optimization</p>
       <p className='font-bold text-black'>Prediction analytics</p>
-      <div class="flex justify-center mt-6">
-        <button class="h-12 w-40 mt-10 rounded-2xl inline-flex text-black bg-green-400  border-0 py-2 px-6 focus:outline-none hover:bg-indigo-600 text-white ">Learn More</button>
+      <div className="flex justify-center mt-6">
+        <button className="h-12 w-40 mt-10 rounded-2xl inline-flex text-black bg-green-400  border-0 py-2 px-6 focus:outline-none hover:bg-indigo-600 text-white ">Learn More</button>
         
       </div>
     </div>
-    <div class="lg:max-w-lg lg:w-full md:w-1/2 w-5/6">
-      <img class=" object-center rounded" alt="hero" src="https://sierra.keydesign.xyz/saas/wp-content/uploads/sites/3/2023/10/sass-widget-10.png"/>
+    <div className=" bg-black lg:max-w-lg lg:w-full md:w-1/2 w-5/6">
+      <img className=" bg-black bg-center object-center rounded" alt="hero" src="https://sierra.keydesign.xyz/saas/wp-content/uploads/sites/3/2023/10/sass-widget-10.png"/>
     </div>
   </div>
 </section>
     </div>
+
+   <div>
+   <section class="text-gray-600 body-font">
+  <div class="container mx-auto flex px-5 py-24 md:flex-row flex-col items-center">
+    <div class="lg:max-w-lg lg:w-full md:w-1/2 w-5/6 mb-10 md:mb-0">
+      <img class="object-cover object-center rounded" alt="hero" src="https://sierra.keydesign.xyz/saas/wp-content/uploads/sites/3/2023/10/sass-widget-12.png"/>
+    </div>
+    <div class="lg:flex-grow md:w-1/2 lg:pl-24 md:pl-16 flex flex-col md:items-start md:text-left items-center text-center">
+    <p className='font-bold text-black'>Seamless workflows</p>
+      <h1 class="title-font sm:text-4xl text-3xl mb-4 font-medium text-gray-900">Powerful and intuitive
+        <br class="hidden lg:inline-block"/><span className='text-green-400'>automation</span> engine
+      </h1>
+      <p class="mb-8 leading-relaxed">Automate repetitive tasks,Streamline processes,and <br class="hidden lg:inline-block"/> enhance productivity without powerful tools</p>
+      <p className='font-bold text-black'>Workflow streamlining</p>
+      <p className='font-bold text-black'>Task prioritization</p>
+      <p className='font-bold text-black'>Notification system</p>
+      <div className="flex justify-center mt-6">
+        <button className="h-12 w-40 mt-10 rounded-2xl inline-flex text-black bg-green-400  border-0 py-2 px-6 focus:outline-none hover:bg-indigo-600 text-white ">Learn More</button>
+        
+      </div>
+    </div>
+  </div>
+</section>
+   </div>
+
+
+   <div className='min-h-[180vh] border-2 hero_bg'>
+   <section class="text-white body-font">
+  <div class="container px-5 py-24 mx-auto">
+    <div class="flex flex-col text-center w-full mb-20">
+      <p>project management</p>
+      <h1 class="sm:text-3xl text-2xl font-medium title-font mb-4 text-white">Your projecthub,<span className='text-green-400'>simplified</span></h1>
+      <p class="lg:w-2/3 mx-auto leading-relaxed text-base">Consolidate your project management tools into one powerful app.Enjoy effortless organization and intelligent automation.</p>
+    </div>
+    <div class="flex flex-wrap -m-4 text-center">
+      <div class="p-4 md:w-1/4 sm:w-1/2 w-full">
+        <div class="border-0 border-gray-200 px-4 py-6 rounded-lg">
+         
+          <h2 class="text-left title-font font-medium text-3xl text-white">Tracking</h2>
+          <p class="text-left leading-relaxed">we create functional and intuitive interfaces and apps for desktop</p>
+        </div>
+      </div>
+      <div class="p-4 md:w-1/4 sm:w-1/2 w-full">
+        <div class="border-0 border-gray-200 px-4 py-6 rounded-lg">
+         
+          <h2 class="text-left title-font font-medium text-3xl text-white">Collaboration</h2>
+          <p class="text-left leading-relaxed">You need a reliable backend application that is fast and scales</p>
+        </div>
+      </div>
+      <div class="p-4 md:w-1/4 sm:w-1/2 w-full">
+        <div class="border-0 border-gray-200 px-4 py-6 rounded-lg">
+         
+          <h2 class="text-left title-font font-medium text-3xl text-white">Workflow</h2>
+          <p class="text-left leading-relaxed">Using an app that scales easily you don't need to create</p>
+        </div>
+      </div>
+      <div class="p-4 md:w-1/4 sm:w-1/2 w-full">
+        <div class="border-0 border-gray-200 px-4 py-6 rounded-lg">
+         
+          <h2 class="text-left title-font font-medium text-3xl text-white">Automation</h2>
+          <p class="text-left leading-relaxed">we'll execute,moniter and improve your campaigns</p>
+        </div>
+      </div>
+    </div>
+  </div>
+</section>
+
+<div className='bg-blur min-h-[60vh] ml-24 mr-24 bg-white/10'>
+<section class="text-gray-600 body-font">
+  <div class="container mx-auto flex px-5 py-24 md:flex-row flex-col items-center">
+    <div class="lg:flex-grow md:w-1/2 lg:pr-24 md:pr-16 flex flex-col md:items-start md:text-left mb-16 md:mb-0 items-center text-center">
+      <p className='text-white'>Lunch with ease</p>
+      <h1 class="text-white title-font sm:text-4xl text-3xl mb-4 font-medium ">Take <span className='text-green-400'>quick action</span> and power your business today</h1>
+      <p class="text-white mb-8 leading-relaxed">Elevate sales and stay ahead in the competitive market by being among the first to benefit from our game-changing solutions </p>
+      <button className="h-12 w-40 mt-10 rounded-2xl inline-flex text-black bg-green-400  border-0 py-2 px-6 focus:outline-none hover:bg-indigo-600 text-white ">Discover More</button>
+    
+    </div>
+    <div class="lg:max-w-lg lg:w-full md:w-1/2 w-5/6">
+      <img class="object-cover object-center rounded" alt="hero" src="https://sierra.keydesign.xyz/saas/wp-content/uploads/sites/3/2023/10/sass-widget-18.png"/>
+    </div>
+  </div>
+ 
+</section>
+
+</div>
+<div className='text-white'>
+    <p className='text-4xl '>stripe asana Google Spotify Trustpilot</p>
+  </div>
    
+   
+   </div>
    </section>
-   
 
    
   )
