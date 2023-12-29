d22249b7844ad5c1c30436fbeb0b409753c67496
arsalan <arsalancity61@gmail.com>
2023-12-29 18:50:43 +0500

    icon

--- src/components/Homepage.js
+++ src/components/Homepage.js
@@ -1,7 +1,9 @@
 import React from 'react';
-import { BsBriefcase } from "react-icons/bs";
+
 import { FaRegCheckCircle } from "react-icons/fa";
 import { FaRegArrowAltCircleRight } from "react-icons/fa";
+import { PiChatsDuotone } from "react-icons/pi";
+import { FcCheckmark } from "react-icons/fc";
 
 
 const Homepage = () => {
@@ -29,67 +31,63 @@ const Homepage = () => {
    
    
     
-    <div className=' bg-slate-100'>
-      <div className='flex gap-5 ml-32 mr-32'>
+    <div className=' bg-slate-100' >
+      <div className='flex gap-5 ml-32 mr-32  '>
 
-      <div className=''> <img className='rounded-2xl ' src="https://sierra.keydesign.xyz/saas/wp-content/uploads/sites/3/2023/10/sass-widget-3.png"/></div>
-      <div className='flex flex-col gap-5  '> <img className='rounded-2xl' src="https://sierra.keydesign.xyz/saas/wp-content/uploads/sites/3/2023/10/sass-widget-6.png"/>
+      <div className='mt-[-72px]'> <img className='rounded-2xl ' src="https://sierra.keydesign.xyz/saas/wp-content/uploads/sites/3/2023/10/sass-widget-3.png "/></div>
+      <div className='flex flex-col gap-5 mt-[-100px]  '> <img className='rounded-2xl' src="https://sierra.keydesign.xyz/saas/wp-content/uploads/sites/3/2023/10/sass-widget-6.png"/>
             <img className='rounded-2xl' src="https://sierra.keydesign.xyz/saas/wp-content/uploads/sites/3/2023/10/sass-widget-7.png"/>
       </div>
-      <div className='flex flex-col gap-5  '> <img className='rounded-2xl' src="https://sierra.keydesign.xyz/saas/wp-content/uploads/sites/3/2023/10/sass-widget-7.png"/>
+      <div className='flex flex-col gap-5 mt-[-100px] '> <img className='rounded-2xl' src="https://sierra.keydesign.xyz/saas/wp-content/uploads/sites/3/2023/10/sass-widget-7.png"/>
             <img className='rounded-2xl' src="https://sierra.keydesign.xyz/saas/wp-content/uploads/sites/3/2023/10/sass-widget-5.png"/>
       </div>
-      <div> <img className='rounded-2xl' src="https://sierra.keydesign.xyz/saas/wp-content/uploads/sites/3/2023/10/sass-widget-9.png"/></div>
+      <div className='mt-[-72px]'> <img className='rounded-2xl' src="https://sierra.keydesign.xyz/saas/wp-content/uploads/sites/3/2023/10/sass-widget-9.png"/></div>
       </div>
    
    
     <section className="text-gray-600 body-font">
   <div className="container px-5 py-24 mx-auto">
     <div className="flex flex-col text-center w-full mb-20">
-      <h5 className="sm:text-1xl mb-4 text-gray-900">Project management essentials</h5>
-      <h1 className=" sm:text-5xl mb-4 text-gray-900">Plan,execute,and track</h1>
-      <h5 className="sm:text-5xl mb-4 text-gray-900">projects of <span className='text-green-400'>any size</span></h5>
-      <h5 className="lg:w-2/3 mx-auto leading-relaxed text-base">Our app offers solid planning tools,seamless collaboration features,robust data protection,and unparalleled software support</h5>
+      <h5 className=" c sm:text-1xl mb-4 text-gray-900">Project management essentials</h5>
+      <h1 className=" f sm:text-5xl mb-4 text-gray-900">Plan,execute,and track</h1>
+      <h5 className=" f sm:text-5xl mb-4 text-gray-900">projects of <span className='text-green-400'>any size</span></h5>
+      <h5 className=" b lg:w-2/3 mx-auto leading-relaxed text-base">Our app offers solid planning tools,seamless collaboration features,<br/> robust data protection,and unparalleled software support</h5>
     </div>
     <div className="flex flex-wrap -m-4 text-center">
       <div className="p-4 md:w-1/4 sm:w-1/2 w-full">
         <div className="border-0 border-gray-200 px-4 py-6 rounded-lg">
-          <svg fill="none" stroke="currentColor" stroke-linecap="round" stroke-linejoin="round" stroke-width="2" className="text-indigo-500 hover:text-green-400 w-12 h-12 mb-3 inline-block" viewBox="0 0 24 24">
-            <path d="M8 17l4 4 4-4m-4-5v9"></path>
-            <path d="M20.88 18.09A5 5 0 0018 9h-1.26A8 8 0 103 16.29"></path>
+          <svg className="text-white mr-32 w-20 rounded-xl  bg-indigo-400 hover:bg-green-400  " viewBox="0 0 20 15">
+          <PiChatsDuotone />
           </svg>
-          <h2 className="text-left title-font font-bold text-1xl  text-gray-900">Solid planning</h2>
-          <p className=" text-left leading-relaxed">From the small stuff to the big picture ,we organize work</p>
+          <h2 className=" d mt-2 text-left title-font   text-gray-900">Solid planning</h2>
+          <p className=" c mt-2 text-left leading-relaxed">From the small stuff to the big picture ,we organize work</p>
         </div>
       </div>
       <div className="p-4 md:w-1/4 sm:w-1/2 w-full">
         <div className="border-0 border-gray-200 px-4 py-6 rounded-lg">
-          <svg fill="none" stroke="currentColor" stroke-linecap="round" stroke-linejoin="round" stroke-width="2" className="text-indigo-500 hover:text-green-400 w-12 h-12 mb-3 inline-block" viewBox="0 0 24 24">
-            <path d="M17 21v-2a4 4 0 00-4-4H5a4 4 0 00-4 4v2"></path>
-            <circle cx="9" cy="7" r="4"></circle>
-            <path d="M23 21v-2a4 4 0 00-3-3.87m-4-12a4 4 0 010 7.75"></path>
+        <svg className="text-white mr-32 w-20 rounded-xl  bg-indigo-400 hover:bg-green-400  " viewBox="0 0 20 15">
+          <PiChatsDuotone />
           </svg>
-          <h2 className=" text-left title-font font-bold text-1xl text-gray-900">Collaboration tools</h2>
-          <p className="text-left leading-relaxed">From the small stuff to the big picture ,we organize work</p>
+          <h2 className=" d mt-2 text-left title-font   text-gray-900">Collaboration tools</h2>
+          <p className=" c mt-2 text-left leading-relaxed">From the small stuff to the big picture ,we organize work</p>
         </div>
       </div>
       <div className="p-4 md:w-1/4 sm:w-1/2 w-full">
         <div className="border-0 border-gray-200 px-4 py-6 rounded-lg">
-          <svg fill="none" stroke="currentColor" stroke-linecap="round" stroke-linejoin="round" stroke-width="2" className=" text-indigo-500 hover:text-green-400 w-12 h-12 mb-3 inline-block" viewBox="0 0 24 24">
-            <path d="M3 18v-6a9 9 0 0118 0v6"></path>
-            <path d="M21 19a2 2 0 01-2 2h-1a2 2 0 01-2-2v-3a2 2 0 012-2h3zM3 19a2 2 0 002 2h1a2 2 0 002-2v-3a2 2 0 00-2-2H3z"></path>
+        <svg className="text-white mr-32 w-20 rounded-xl  bg-indigo-400 hover:bg-green-400  " viewBox="0 0 20 15">
+          <PiChatsDuotone />
           </svg>
-          <h2 className="text-left title-font font-bold text-1xl text-gray-900">Data protection</h2>
-          <p className="text-left leading-relaxed">From the small stuff to the big picture ,we organize work</p>
+          <h2 className=" mt-2 d text-left title-font   text-gray-900">Data protection</h2>
+          <p className=" mt-2 c text-left leading-relaxed">From the small stuff to the big picture ,we organize work</p>
         </div>
       </div>
       <div className="p-4 md:w-1/4 sm:w-1/2 w-full">
         <div className="border-0 border-gray-200 px-4 py-6 rounded-lg">
-          <svg fill="none" stroke="currentColor" stroke-linecap="round" stroke-linejoin="round" stroke-width="2" className="text-indigo-500 hover:text-green-400 w-12 h-12 mb-3 inline-block" viewBox="0 0 24 24">
-            <path d="M12 22s8-4 8-10V5l-8-3-8 3v7c0 6 8 10 8 10z"></path>
+        <svg className="text-white mr-32 w-20 rounded-xl  bg-indigo-400 hover:bg-green-400  " viewBox="0 0 20 15">
+          <PiChatsDuotone />
           </svg>
-          <h2 className="text-left title-font font-bold text-1xl text-gray-900">Software support</h2>
-          <p className="text-left leading-relaxed">From the small stuff to the big picture ,we organize work</p>
+          <h2 className=" mt-2 d text-left title-font   text-gray-900">Software support</h2>
+          <p className=" mt-2 c text-left leading-relaxed">From the small stuff to the big picture ,we organize work</p>
         </div>
       </div>
     </div>
@@ -101,21 +99,21 @@ const Homepage = () => {
     <section className="text-gray-600 body-font">
   <div className="container mx-auto flex px-5 py-24 md:flex-row flex-col items-center">
     <div className="lg:flex-grow md:w-1/2 lg:pr-24 md:pr-16 flex flex-col md:items-start md:text-left mb-16 md:mb-0 items-center text-center">
-      <h1 className='font-bold text-black'>Real-time analytics</h1>
-      <h1 className="  sm:text-4xl text-3xl mb-4 font-large text-gray-900">Check project progress
+      <h1 className=' c mt-0 text-black'>Real-time analytics</h1>
+      <h1 className="f mt-2  sm:text-4xl  mb-4  text-gray-900">Check project progress
         <br className="hidden lg:inline-block"/>and <span className='text-green-400'>performance</span> 
       </h1>
-      <p className="mb-8 leading-relaxed">Boost productivity,optimize workflows,and make  <br className="hidden lg:inline-block"/> data-driven decisions effortlessly</p>
-      <p className='font-bold text-black'>Performance tracking</p>
-      <p className='font-bold text-black'>Resource optimization</p>
-      <p className='font-bold text-black'>Prediction analytics</p>
+      <p className="b mb-8 leading-relaxed">Boost productivity,optimize workflows,and make  <br className="hidden lg:inline-block"/> data-driven decisions effortlessly</p>
+      <p className='b  text-black gap-2 flex '><FcCheckmark />Performance tracking</p>
+      <p className='b  text-black gap-2 flex'><FcCheckmark />Resource optimization</p>
+      <p className='b  text-black gap-2 flex'><FcCheckmark />Prediction analytics</p>
       <div className="flex justify-center mt-6">
-        <button className="h-12 w-40 mt-10 rounded-2xl inline-flex text-black bg-green-400  border-0 py-2 px-6 focus:outline-none hover:bg-indigo-600 text-white ">Learn More</button>
+        <button className="h-12 w-40 mt-6 rounded-3xl inline-flex text-black bg-green-400  border-0 py-2 px-6 focus:outline-none hover:bg-indigo-600 text-white ">Learn More</button>
         
       </div>
     </div>
-    <div className=" bg-black lg:max-w-lg lg:w-full md:w-1/2 w-5/6">
-      <img className=" bg-black bg-center object-center rounded" alt="hero" src="https://sierra.keydesign.xyz/saas/wp-content/uploads/sites/3/2023/10/sass-widget-10.png"/>
+    <div className="  lg:max-w-lg lg:w-full md:w-1/2 w-5/6">
+      <img className="  p-14 bg-gray-100 rounded-2xl bg-center object-center " alt="hero" src="https://sierra.keydesign.xyz/saas/wp-content/uploads/sites/3/2023/10/sass-widget-10.png"/>
     </div>
   </div>
 </section>
@@ -125,19 +123,19 @@ const Homepage = () => {
    <section class="text-gray-600 body-font">
   <div class="container mx-auto flex px-5 py-24 md:flex-row flex-col items-center">
     <div class="lg:max-w-lg lg:w-full md:w-1/2 w-5/6 mb-10 md:mb-0">
-      <img class="object-cover object-center rounded" alt="hero" src="https://sierra.keydesign.xyz/saas/wp-content/uploads/sites/3/2023/10/sass-widget-12.png"/>
+      <img class=" p-14 bg-gray-100 object-cover object-center rounded-2xl" alt="hero" src="https://sierra.keydesign.xyz/saas/wp-content/uploads/sites/3/2023/10/sass-widget-12.png"/>
     </div>
     <div class="lg:flex-grow md:w-1/2 lg:pl-24 md:pl-16 flex flex-col md:items-start md:text-left items-center text-center">
-    <p className='font-bold text-black'>Seamless workflows</p>
-      <h1 class="title-font sm:text-4xl text-3xl mb-4 font-medium text-gray-900">Powerful and intuitive
+    <p className='c text-black'>Seamless workflows</p>
+      <h1 class=" f mt-2 sm:text-4xl  mb-4  text-gray-900">Powerful and intuitive
         <br class="hidden lg:inline-block"/><span className='text-green-400'>automation</span> engine
       </h1>
-      <p class="mb-8 leading-relaxed">Automate repetitive tasks,Streamline processes,and <br class="hidden lg:inline-block"/> enhance productivity without powerful tools</p>
-      <p className='font-bold text-black'>Workflow streamlining</p>
-      <p className='font-bold text-black'>Task prioritization</p>
-      <p className='font-bold text-black'>Notification system</p>
+      <p class=" b mb-8 leading-relaxed">Automate repetitive tasks,Streamline processes,and <br class="hidden lg:inline-block"/> enhance productivity without powerful tools</p>
+      <p className='b text-black gap-2 flex'><FcCheckmark />Workflow streamlining</p>
+      <p className='b text-black gap-2 flex'><FcCheckmark />Task prioritization</p>
+      <p className='b text-black gap-2 flex'><FcCheckmark />Notification system</p>
       <div className="flex justify-center mt-6">
-        <button className="h-12 w-40 mt-10 rounded-2xl inline-flex text-black bg-green-400  border-0 py-2 px-6 focus:outline-none hover:bg-indigo-600 text-white ">Learn More</button>
+        <button className="h-12 w-40 mt-6 rounded-3xl inline-flex text-black bg-green-400  border-0 py-2 px-6 focus:outline-none hover:bg-indigo-600 text-white ">Learn More</button>
         
       </div>
     </div>
@@ -287,46 +285,54 @@ const Homepage = () => {
 
 
 <div className='h-screen w-screen bg-gray-100'>
-<section className="text-gray-600 body-font ">
-  <div className="container mx-auto flex px-5 py-24 md:flex-row flex-col  ">
-    <div className="mt-20 lg:flex-grow md:w-1/2 lg:pr-24 md:pr-16 flex flex-col md:items-start md:text-left mb-16 md:mb-0 items-center text-center">
-      <h1 className='font-bold text-black'>Transforming teamwork</h1>
-      <h1 className="  sm:text-4xl  mb-4 font-large text-gray-900">Join <span className='text-green-400'>4000+</span>
-        <br className="hidden lg:inline-block"/>businesses in our <br className="hidden lg:inline-block"/>community 
+<section className="text-gray-600 ">
+  <div className="container mx-auto flex  py-24 md:flex-row flex-col  ">
+    <div className="mt-20  flex flex-col md:items-start md:text-left ">
+      <h1 className=' text-black'>Transforming teamwork</h1>
+      <h1 className="mt-4 f  sm:text-4xl  mb-4  text-black">Join <span className='text-green-400'>4000+</span>
+       <br/> businesses in our community 
       </h1>
-       <p>Discover how various industry teams  <br className="hidden lg:inline-block"/>benefit from our software,optimizing<br className="hidden lg:inline-block"/>their workflows,and improving efficiency.</p>
+      
+       <p className='b text-gray-400'>Discover how various industry teams  <br className="hidden lg:inline-block"/>benefit from our software,optimizing<br className="hidden lg:inline-block"/>their workflows,and improving efficiency.</p>
       <div className="flex justify-center mt-6">
         <button className="h-12 w-40 mt-10 rounded-2xl inline-flex text-black bg-green-400  border-0 py-2 px-6 focus:outline-none hover:bg-indigo-600 text-white ">Discover More</button>
         
       </div>
     </div>
-  <div className='flex-row mt-20 ml-32'>
+  <div className='flex-row mt-20 ml-48'>
           <div className='flex'>
-          <div className=' mb-80 mr-20 '>
-           <p className=' text-left font-bold'> Marketing teams</p>
-           <p className='text-left text-gray-400'>Enhance campaign planning,collaboration,and performance tracking</p>
+          <div className=' mb-12 mr-20 '>
+          <svg className="mb-4 text-white mr-32 w-20 rounded-xl  bg-indigo-400 hover:bg-green-400  " viewBox="0 0 20 15">
+          <PiChatsDuotone />
+          </svg>
+           <p className=' d mb-4 text-left text-black'> Marketing teams</p>
+           <p className=' c text-left text-gray-400'>Enhance campaign planning,collaboration,and performance tracking</p>
             </div>
           <div>
-           <p className='text-left font-bold'>Project management</p>
-           <p className='text-left text-gray-400'>Stremline task assignment progress tracking, and resource allocation</p> 
+          <svg className="mb-4 text-white mr-32 w-20 rounded-xl  bg-indigo-400 hover:bg-green-400  " viewBox="0 0 20 15">
+          <PiChatsDuotone />
+          </svg>
+           <p className=' d mb-4 text-left text-black'>Project management</p>
+           <p className='c text-left text-gray-400'>Stremline task assignment progress tracking, and resource allocation</p> 
             </div>
           </div>
           
 
        <div className='flex'>
        <div className=' mb-72 mr-24'>
-      <p className=''><BsBriefcase /></p> 
-       <p className='text-left font-bold'>Strategic planning</p>
-       <p className='text-left text-gray-400'>Facilitate data-driven decision-making,scenario analysis,and goal tracking.</p>
+       <svg className="mb-4 text-white mr-32 w-20 rounded-xl  bg-indigo-400 hover:bg-green-400  " viewBox="0 0 20 15">
+          <PiChatsDuotone />
+          </svg>
+       <p className='d mb-4 text-left text-black'>Strategic planning</p>
+       <p className='c text-left text-gray-400'>Facilitate data-driven decision-making,scenario analysis,and goal tracking.</p>
 
         </div>
        <div>
-       <svg fill="none" stroke="currentColor" stroke-linecap="round" stroke-linejoin="round" stroke-width="5" className="text-indigo-500 hover:text-green-400 w-12 h-12 mb-3 inline-block" viewBox="0 0 24 24">
-            <path d="M8 17l4 4 4-4m-4-5v9"></path>
-            <path d="M20.88 18.09A5 5 0 0018 9h-1.26A8 8 0 103 16.29"></path>
+       <svg className="mb-4 text-white mr-32 w-20 rounded-xl  bg-indigo-400 hover:bg-green-400  " viewBox="0 0 20 15">
+          <PiChatsDuotone />
           </svg>
-        <p className='text-left font-bold'>Operation teams</p>
-        <p className='text-left text-gray-400'>Optimize process automation,documentation,and communication.</p>
+        <p className='d mb-4 text-left text-black'>Operation teams</p>
+        <p className='c text-left text-gray-400'>Optimize process automation,documentation,and communication.</p>
         </div>
         </div>   
          
